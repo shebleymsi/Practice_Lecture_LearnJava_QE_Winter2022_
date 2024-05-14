3eef116c36c2f3497669fc4e0a994143acbc3f55
Shebleymsi <Shebley2022@gmail.com>
2024-05-14 15:36:27 -0400

    unittest: demo, hook --> class updated

--- src/unitTesting/Demo.java
+++ src/unitTesting/Demo.java
@@ -9,58 +9,67 @@ import java.io.IOException;
 
 public class Demo extends Hooks {
 
+    //==================================== main() =========================================
+    // we know that without main method java can not execute the code
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
 
-//    public static void main(String[] args) {
-//        System.out.println("Hello");
-//    }
-
+    //==================================== @Test/ main() =================================
 
     // if you use @Test annotation on top of any method then it will consider as a Test Method/ Test case method/ Test case
-
+    // Test-case
     @Test
-    @Ignore
     public void display() {
         System.out.println("This is Display");
     }
 
-
+    //================================== @Ignore =========================================
     @Test
+    @Ignore // @Ignore for JUnit 4.13 and @Disabled for JUnit 5
     public void display1() {
         System.out.println("This is Display1");
     }
+
+    //================================== (timeout = 500L) ===============================
 
     @Test(timeout = 500L)  //@Order()  // here, L = millisecond
     public void display4() {
         System.out.println("This is Display1");
     }
 
+    // ================================ Exception handheld by JUnit =====================
     @Test(expected = FileNotFoundException.class)
     public void display5() {
         System.out.println("This is Display1");
     }
+    // should watch:
+    // https://youtu.be/3N7-pwSx-HA?si=eeCNwvVksTM-vm3a
 
-
+    // ================================ commenting ======================================
     /**
      * This method is for displaying information
-     * Author: Mahmud
+     * Author: Shebley
      */
     public void display3() {
         System.out.println("This is Display1");
     }
 
-
+    // ================================ commenting =====================================
     /**
      * This method is for display purpose
      *
-     * @param st Author: Mahmud
+     * @param st Author: Shebley
      */
-    public void display3(String st) {
+
+    public void display3(String st)  {
         System.out.println("This is Display1");
     }
 
 
+    // ================================ Verification: =================================
     @Test
-    public void doSum() {
+    public void doSum() {  // v36 05.21.00
         int a = 5;
         int b = 6;
         int c = a - b;
@@ -70,11 +79,15 @@ public class Demo extends Hooks {
 //        } else {
 //            System.out.println("Test fail");
 //        }
+//      // c is actual result and 11 is expected result
 
-        // Verification
-        Assert.assertEquals("Test Fail", 11, c);
+
+        // instead of our if-else we can use "Verification"
+        // without Verification a Test-case is called incomplete Test-case
+        // Verification:
+        Assert.assertEquals("Test fail", 11, c);
         // Assert.assertEquals("","","");
-
+        // smart way to check (message, expected, actual)
 
     }
 
